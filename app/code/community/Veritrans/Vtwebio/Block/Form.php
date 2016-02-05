481abd8e7758259f73f504116810215a2993e567
Harry <harry.pujianto@veritrans.co.id>
2016-02-05 16:13:24 +0700

    add offline installment feature

---
+++ app/code/community/Veritrans/Vtwebio/Block/Form.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Veritrans VT Web form block
+ *
+ * @category   Mage
+ * @package    Mage_Veritrans_VtWeb_Block_Form
+ * @author     Kisman Hong, plihplih.com
+ * when Veritrans payment method is chosen, vtweb/form.phtml template will be rendered through this class.
+ */
+class Veritrans_Vtwebio_Block_Form extends Mage_Payment_Block_Form
+{
+    
+    protected function _construct()
+    {
+        parent::_construct();
+
+		$this->setFormMessage(Mage::helper('vtwebio/data')->_getFormMessage());
+        $this->setTemplate('vtwebio/form.phtml');
+    }
+}
+?>
